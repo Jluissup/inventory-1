4be788a1caca4bac28cf12becc38dcf79c18126f
Jluissup <asistente.trafic@gmail.com>
2025-04-30 13:20:45 -0700

    fix some issue

--- resources/assets/js/components/product/ViewProduct.vue
+++ resources/assets/js/components/product/ViewProduct.vue
@@ -29,7 +29,7 @@
         <table class="table table-condensed table-hover">
           <thead>
             <tr>
-              <th>Categorías123</th>
+              <th>Categorias</th>
               <th>Nombre</th>
               <th>Detalles</th>
               <th>Editar</th>
